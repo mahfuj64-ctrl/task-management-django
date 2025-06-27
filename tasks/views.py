9ae44a311a15470a6da534db4d68e7804c640b43
Mahfuj <bmmahfuj70264@gmail.com>
2025-06-28 01:37:53 +0600

    Updated views and urls, added dashboard templates

--- tasks/views.py
+++ tasks/views.py
@@ -4,7 +4,7 @@ from django.http import HttpResponse
 # Create your views here.
 
 def home(request):
-        return HttpResponse("Welcome to the task management system")
+        return render(request, "home.html")
 
 def contact(request):
         return HttpResponse("<h1 style='color: blue'> This is contact page</h1>")
@@ -16,3 +16,4 @@ def show_specific_task(request,id):
         print('id',id)
         print('id type', type(id))
         return HttpResponse(f"This is specific task page{id}")
+
