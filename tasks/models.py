fe7ad7417702bf6858a0f4ce5d16f0616bf3fd04
Mahfuj <bmmahfuj70264@gmail.com>
2025-06-30 01:24:23 +0600

    Added migration and updated models

--- tasks/models.py
+++ tasks/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 class Task(models.Model):
-    titile = models.CharField(max_length=250)
+    title = models.CharField(max_length=250)
     description = models.TextField()
     due_date = models.DateField()
     is_completed = models.BooleanField(default=False)
